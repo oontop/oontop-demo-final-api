db5969932e9dca052afa595330f53031d50c0ef0
Eliyahu <eliyahuofficialmusic@gmail.com>
2024-09-03 21:23:00 +0300

    update configDevEnv

--- config/index.ts
+++ config/index.ts
@@ -5,16 +5,21 @@ const configDevEnv = () => {
     config({ path: "config/dev.env" });
 
     const mode = process.env.NODE_ENV;
+    const dbType = process.env.DATABASE_TYPE;
 
+    if (dbType === 'firebase') {
+        console.log('Using Firebase as the database.');
+    } else if (dbType === 'mongo') {
+        console.log('Using MongoDB as the database.');
+    } else {
+        console.log('Unknown database type, defaulting to MongoDB.');
+    }
 
     if (mode === 'additionalMode') {
-
         console.log('Additional mode is active, but no configuration is loaded.');
     } else {
-
         config({ path: `config/${mode}.env` });
     }
 };
 
 export default configDevEnv;
-
